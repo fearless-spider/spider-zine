9cc3ad916dd116f19de848710730d83deb95fac3
Przemyslaw Pajak <office@fearlessspider.com>
2020-12-14 16:00:31 +0100

    Add comments

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <iostream> // IO stream library
 
+// Body of the program
 int main() {
+  // Send "Hello World!" to output with end line
   std::cout << "Hello World!" << std::endl;
+
+  return 0;
 }
